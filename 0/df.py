5861b68a81eec65b99b0c12f7301169fa978f0aa
unknown <lodhel@yahoo.com>
2019-04-19 19:42:39 +0300

    first commit

---
+++ 0/df.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('https://stepik.org/media/attachments/course/4852/titanic.csv')
+
+print(df.index)
+print(df.shape)
+print(df.size)
+print(df.columns)
+print(df.dtypes)
+print(df.get_dtype_counts())
+
+print(len(df.index))
